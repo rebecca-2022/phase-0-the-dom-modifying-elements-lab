a5f48bebc354e2325e044a14dc7c890ded6daa15
rebecca-2022 <rebecca.njeri@student.moringaschool.com>
2022-08-05 00:16:32 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const element = document.getElementById("main");
+element.style.height = "300px";
+element.style.backgroundColor = "#27647B";
+element.textContent = "You've changed what's on the screen!";
+
+  
